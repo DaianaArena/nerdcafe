f293d88502a2079ae9c57a630e18482c81d00d37
Daiana <arenadaianam@gmail.com>
2023-07-28 05:47:55 -0300

    debbugin audio player

--- src/app/Dashboard/page.tsx
+++ src/app/Dashboard/page.tsx
@@ -19,18 +19,7 @@ export default function Dashboard () {
  
   //<Error404/>
  
-  const [src, setSrc] = useState(
-    "https://audioplayer.madza.dev/Madza-Chords_of_Life.mp3"
-  );
-
-  const changeSrc = () => {
-    setSrc((prev) =>
-      prev ==
-      "https://audioplayer.madza.dev/Madza-Late_Night_Drive.mp3"
-        ? "https://audioplayer.madza.dev/Madza-Chords_of_Life.mp3"
-        : "https://audioplayer.madza.dev/Madza-Persistence.mp3"
-    );
-  };
+  
 
   return (
     <>
@@ -43,39 +32,63 @@ export default function Dashboard () {
           <iframe className="px-4 sm:static sm:px-6 lg:px-8" src="https://open.spotify.com/embed/playlist/1tVd8oXBghV0IjtJHX0wSv?utm_source=generator&theme=0" width="100%" height="352" frameBorder="0"  allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
         </div> 
 
-        <div className="flex-1 h-full bg-green-400">
+        <div className="flex-1  flex flex-col justify-center items-end h-full bg-green-400">
           a
 
-          <div className="w-full p-4 shadow-xl ring-1 ring-zinc-900 ring-opacity-10 rounded-md bg-gray-400">
+          <div className="w-1/2 p-2 shadow-xl ring-1 ring-zinc-900 ring-opacity-10 rounded-md bg-gray-700">
       <PlayerProvider
-        src={src}
+        src={"https://audioplayer.madza.dev/Madza-Chords_of_Life.mp3"}
         loop={true}
-        onEnded={() => {
-          changeSrc();
-        }}
         onPause={() => {}}
         onPlay={() => {}}
       >
         <Player>
           {(context) => {
             return (
-              <div className="w-full max-w-lg mt-10">
-                <PlayerSlider
-                  downloadProgress={context.downloadProgress}
-                  onChange={context.onSliderChange}
-                  progress={context.progress}
-                />
-                <button onClick={context.togglePlay}>
-                  {context.isPlaying ? <FaPause/> : <FaPlay/>}
-                </button>
-                <VolumeSlider
-                  onChange={context.onSliderVolumeChange}
-                  volume={context.volume}
-                />
-                <button onClick={context.toggleMute}>
-                  {context.mute.state == "muted" ?  <FaVolumeMute/> : <FaVolumeUp/> }
-                </button>
-              </div>
+              <>
+                <div className="w-full flex  justify-center items-center">
+                
+
+                  <PlayerSlider
+                    downloadProgress={context.downloadProgress}
+                    onChange={context.onSliderChange}
+                    progress={context.progress}
+                  />
+                </div>
+
+                <div className="w-full flex-col sm:flex-row gap-y-2 sm:gap-y-0 items-start justify-between flex mt-2 sm:items-center">
+                  <div className='flex items-center'>
+
+                    <div className='ml-2'>
+                      <p className='font-semibold text-white'>Rain sounds</p>
+                    </div>
+                  </div>
+
+                  <button onClick={context.togglePlay}>
+                    {context.isPlaying ? <FaPause className=' text-white'/> : <FaPlay className=' text-white'/>}
+                  </button>
+
+                  <div className='flex justify-between   items-center'>
+                  
+
+                  <div className='flex justify-end   items-center'>
+                  <button className='mr-2' onClick={context.toggleMute}>
+                    {context.mute.state == "muted" ?  <FaVolumeMute className=' text-white'/> : <FaVolumeUp className=' text-white'/> }
+                  </button>
+
+                  <div className="w-32 ">
+                  <VolumeSlider
+                    onChange={context.onSliderVolumeChange}
+                    volume={context.volume}
+                  />
+                  </div>
+                  </div>
+                  </div>
+                
+                </div>
+
+                
+              </>
             );
           }}
         </Player>
@@ -95,3 +108,16 @@ export default function Dashboard () {
    
   )
 }
+/*
+
+<div className="w-32 flex max-w-lg mt-10 justify-center items-center">
+                <VolumeSlider
+                  onChange={context.onSliderVolumeChange}
+                  volume={context.volume}
+                />
+                <button onClick={context.toggleMute}>
+                  {context.mute.state == "muted" ?  <FaVolumeMute/> : <FaVolumeUp/> }
+                </button>
+              </div>
+
+*/
