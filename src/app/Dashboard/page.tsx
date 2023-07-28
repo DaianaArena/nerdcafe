522a861c18371a484ac257e6df16ac71ee9b8c92
Daiana <arenadaianam@gmail.com>
2023-07-28 06:17:37 -0300

    dashboard update

--- src/app/Dashboard/page.tsx
+++ src/app/Dashboard/page.tsx
@@ -5,14 +5,10 @@ import Error404 from '../components/Error404'
 // Define the Inter font with the Latin subset
 import {FaPlay, FaPause, FaVolumeUp, FaVolumeMute} from 'react-icons/fa';
 const inter = Inter({ subsets: ['latin'] })
-import {
-  PlayerProvider,
-  Player,
-  PlayerSlider,
-  VolumeSlider,
-} from "headless-audioplayer-react";
-import "headless-audioplayer-react/dist/cjs/css/slider.css";
-import { useState } from "react";
+
+import Audioplayer from '../components/Audioplayer';
+
+
 
 export default function Dashboard () {
  
@@ -24,78 +20,22 @@ export default function Dashboard () {
   return (
     <>
     
-    <div className="flex-1 flex justify-center items-center w-full relative overflow-hidden mx-auto max-w-7xl px-4 sm:static sm:px-6 lg:px-8 bg-red-400">
+    <div className="flex-1 flex justify-center items-center w-full relative overflow-hidden mx-auto max-w-7xl px-4 sm:static sm:px-6 lg:px-8 ">
 
-      <div className="flex w-full h-full justify-center items-center bg-blue-400" >
+      <div className="flex w-full h-full justify-center items-center " >
 
         <div className=" w-80">
           <iframe className="px-4 sm:static sm:px-6 lg:px-8" src="https://open.spotify.com/embed/playlist/1tVd8oXBghV0IjtJHX0wSv?utm_source=generator&theme=0" width="100%" height="352" frameBorder="0"  allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
         </div> 
 
-        <div className="flex-1  flex flex-col justify-center items-end h-full bg-green-400">
-          a
-
-          <div className="w-1/2 p-2 shadow-xl ring-1 ring-zinc-900 ring-opacity-10 rounded-md bg-gray-700">
-      <PlayerProvider
-        src={"https://audioplayer.madza.dev/Madza-Chords_of_Life.mp3"}
-        loop={true}
-        onPause={() => {}}
-        onPlay={() => {}}
-      >
-        <Player>
-          {(context) => {
-            return (
-              <>
-                <div className="w-full flex  justify-center items-center">
-                
-
-                  <PlayerSlider
-                    downloadProgress={context.downloadProgress}
-                    onChange={context.onSliderChange}
-                    progress={context.progress}
-                  />
-                </div>
-
-                <div className="w-full flex-col sm:flex-row gap-y-2 sm:gap-y-0 items-start justify-between flex mt-2 sm:items-center">
-                  <div className='flex items-center'>
-
-                    <div className='ml-2'>
-                      <p className='font-semibold text-white'>Rain sounds</p>
-                    </div>
-                  </div>
-
-                  <button onClick={context.togglePlay}>
-                    {context.isPlaying ? <FaPause className=' text-white'/> : <FaPlay className=' text-white'/>}
-                  </button>
-
-                  <div className='flex justify-between   items-center'>
-                  
-
-                  <div className='flex justify-end   items-center'>
-                  <button className='mr-2' onClick={context.toggleMute}>
-                    {context.mute.state == "muted" ?  <FaVolumeMute className=' text-white'/> : <FaVolumeUp className=' text-white'/> }
-                  </button>
-
-                  <div className="w-32 ">
-                  <VolumeSlider
-                    onChange={context.onSliderVolumeChange}
-                    volume={context.volume}
-                  />
-                  </div>
-                  </div>
-                  </div>
-                
-                </div>
-
-                
-              </>
-            );
-          }}
-        </Player>
-      </PlayerProvider>
-      
-    </div>
+        <div className="flex-1  flex flex-col justify-center items-end h-full ">
           
+
+          <Audioplayer   src={"https://audioplayer.madza.dev/Madza-Chords_of_Life.mp3"} name={"Rain sound"}/>
+          
+          <Audioplayer src={"https://audioplayer.madza.dev/Madza-Chords_of_Life.mp3"} name={"Coffee shop sound"}/>
+          
+          <Audioplayer src={"https://audioplayer.madza.dev/Madza-Chords_of_Life.mp3"} name={"Fireplace sound"}/>
           
           
         </div>
---
+++ src/app/components/Audioplayer.tsx
@@ -0,0 +1,82 @@
+import React from 'react'
+import {FaPlay, FaPause, FaVolumeUp, FaVolumeMute} from 'react-icons/fa';
+
+import {
+  PlayerProvider,
+  Player,
+  PlayerSlider,
+  VolumeSlider,
+} from "headless-audioplayer-react";
+import "headless-audioplayer-react/dist/cjs/css/slider.css";
+
+interface AudioplayerProps {
+    name: string;
+    src: string; 
+  }
+
+function Audioplayer(props) {  
+  return (
+    <div className="w-1/2  my-4 p-2 shadow-xl ring-1 ring-zinc-900 ring-opacity-10 rounded-md bg-custom-gray">
+      <PlayerProvider
+        src={props.src}
+        loop={true}
+        onPause={() => {}}
+        onPlay={() => {}}
+      >
+        <Player>
+          {(context) => {
+            return (
+              <>
+                <div className="w-full flex  justify-center items-center">
+                
+
+                  <PlayerSlider
+                    downloadProgress={context.downloadProgress}
+                    onChange={context.onSliderChange}
+                    progress={context.progress}
+                  />
+                </div>
+
+                <div className="w-full flex-col sm:flex-row gap-y-2 sm:gap-y-0 items-start justify-between flex mt-2 sm:items-center">
+                  <div className='flex items-center'>
+
+                    <div className='ml-2'>
+                      <p className='font-semibold w-40 text-white'>{props.name}</p>
+                    </div>
+                  </div>
+
+                  <button onClick={context.togglePlay}>
+                    {context.isPlaying ? <FaPause className=' text-white'/> : <FaPlay className=' text-white'/>}
+                  </button>
+
+                  <div className='flex justify-between   items-center'>
+                  
+
+                  <div className='flex justify-end   items-center'>
+                  <button className='mr-2' onClick={context.toggleMute}>
+                    {context.mute.state == "muted" ?  <FaVolumeMute className=' text-white'/> : <FaVolumeUp className=' text-white'/> }
+                  </button>
+
+                  <div className="w-32 ">
+                  <VolumeSlider
+                    onChange={context.onSliderVolumeChange}
+                    volume={context.volume}
+                  />
+                  </div>
+                  </div>
+                  </div>
+                
+                </div>
+
+                
+              </>
+            );
+          }}
+        </Player>
+      </PlayerProvider>
+      
+    </div>
+  )
+}
+
+export default Audioplayer
