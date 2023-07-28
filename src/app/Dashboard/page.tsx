555265daaa5493a7015c043d2228e3ff8872615e
Daiana <arenadaianam@gmail.com>
2023-07-28 05:05:23 -0300

    adding players

--- src/app/Dashboard/page.tsx
+++ src/app/Dashboard/page.tsx
@@ -4,14 +4,32 @@ import { Inter } from 'next/font/google'
 import Error404 from '../components/Error404'
 // Define the Inter font with the Latin subset
 const inter = Inter({ subsets: ['latin'] })
+import {
+  PlayerProvider,
+  Player,
+  PlayerSlider,
+  VolumeSlider,
+} from "headless-audioplayer-react";
+import "headless-audioplayer-react/dist/cjs/css/slider.css";
+import { useState } from "react";
 
 export default function Dashboard () {
  
  
   //<Error404/>
  
-  
-  
+  const [src, setSrc] = useState(
+    "https://audioplayer.madza.dev/Madza-Chords_of_Life.mp3"
+  );
+
+  const changeSrc = () => {
+    setSrc((prev) =>
+      prev ==
+      "https://audioplayer.madza.dev/Madza-Late_Night_Drive.mp3"
+        ? "https://audioplayer.madza.dev/Madza-Chords_of_Life.mp3"
+        : "https://audioplayer.madza.dev/Madza-Persistence.mp3"
+    );
+  };
 
   return (
     <>
@@ -27,6 +45,48 @@ export default function Dashboard () {
         <div className="flex-1 h-full bg-green-400">
           a
 
+          <div className="w-full p-4 shadow-xl ring-1 ring-zinc-900 ring-opacity-10 rounded-md bg-gray-400">
+      <PlayerProvider
+        src={src}
+        loop={true}
+        onEnded={() => {
+          changeSrc();
+        }}
+        onPause={() => {}}
+        onPlay={() => {}}
+      >
+        <Player>
+          {(context) => {
+            return (
+              <div className="w-full max-w-lg mt-10">
+                <PlayerSlider
+                  downloadProgress={context.downloadProgress}
+                  onChange={context.onSliderChange}
+                  progress={context.progress}
+                />
+                <button onClick={context.togglePlay}>
+                  {context.isPlaying ? "Pause" : "Play"}
+                </button>
+                <p>{context.timestamp.current}</p>
+                <p>{context.timestamp.total}</p>
+                <button onClick={context.increaseVolume}>+1</button>
+                <button onClick={context.decreaseVolume}>-1</button>
+                <VolumeSlider
+                  onChange={context.onSliderVolumeChange}
+                  volume={context.volume}
+                />
+                <button onClick={context.toggleMute}>
+                  {context.mute.state == "muted" ? "Unmute" : "Mute"}
+                </button>
+              </div>
+            );
+          }}
+        </Player>
+      </PlayerProvider>
+      <button onClick={changeSrc}>Change src</button>
+    </div>
+          
+          
           
         </div>
       </div>    
